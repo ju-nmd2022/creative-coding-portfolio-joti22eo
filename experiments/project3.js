78b6990c4c6f4fb72e86f60b09c0e70870a31c49
Tilde <joti22eo@student.ju.se>
2024-08-30 11:14:10 +0200

    Project 3 variation - 02

--- experiments/project3.js
+++ experiments/project3.js
@@ -4,7 +4,7 @@ function setup() {
 
 // The following 7 lines of code are from ChatGPT
 // Draws random ellipses within the ellipse boundary
-function drawRandomEllipse(centerX, centerY, width) {
+function drawRandomRect(centerX, centerY, width) {
   // Generate a random point inside the ellipse
   let angle = random(TWO_PI);
   let radius = sqrt(random(1)) * (width / 2);
@@ -16,8 +16,8 @@ function drawRandomEllipse(centerX, centerY, width) {
   let c = random(120, 220); // Color
 
   // Draw the ellipses at the random point
-  fill(255, c, c);
-  ellipse(x, y, r);
+  fill(c, c, 255);
+  rect(x, y, r);
 }
 
 function draw() {
@@ -29,13 +29,13 @@ function draw() {
   const centerX = width / 2;
   const centerY = height / 2;
   // Width & height
-  const ellipseWidth = 600;
-  const ellipseHeight = 300;
+  const rectWidth = 600;
+  const rectHeight = 300;
   // Number of ellipses
-  const numEllipses = 260;
+  const numRects = 260;
 
-  for (let i = 0; i < numEllipses; i++) {
-    drawRandomEllipse(centerX, centerY, ellipseWidth, ellipseHeight);
+  for (let i = 0; i < numRects; i++) {
+    drawRandomRect(centerX, centerY, rectWidth, rectHeight);
   }
 
   noLoop();
