00e1321fb72d483c4f90e63933d9b474753eb12b
Tilde <joti22eo@student.ju.se>
2024-09-03 08:16:44 +0200

    Updated project 2 variation 2

--- experiments/project2-var2.js
+++ experiments/project2-var2.js
@@ -1,25 +1,22 @@
 function setup() {
   createCanvas(innerWidth, innerHeight);
 }
-
 function randomLines() {
   // The following 4 lines of code are from ChatGPT
   let x1 = random(width);
   let y1 = random(height);
-  let r = random(20, 60);
-  let c = random(120, 220);
-
-  fill(255, c, c);
-  ellipse(x1, y1, r);
+  let x2 = random(width);
+  let y2 = random(height);
+  line(x1, y1, x2, y2);
 }
 
 function draw() {
   background(250, 246, 235);
-  noStroke();
+  stroke(0, 0, 255);
+  strokeWeight(16);
 
   for (let i = 0; i < 160; i++) {
     randomLines();
   }
-
   noLoop();
 }
